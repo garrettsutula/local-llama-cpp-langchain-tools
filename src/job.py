2f4093f42eb4109db9733e738a2444db38d25fe0
Garrett Sutula <garrettsutula@gmail.com>
2023-06-20 22:11:52 -0400

    Add job concept and script

---
+++ src/job.py
@@ -0,0 +1,33 @@
+import yaml
+
+from cmd_args import get_args
+from model import loadModel
+from prompt import loadFewShot, runPrompt
+
+
+def runJob(jobFileName: str):
+  jobSettings = None
+  with open(f"./jobs/{jobFileName}", "r") as stream:
+      try:
+          jobSettings = yaml.safe_load(stream)
+      except yaml.YAMLError as e:
+          print(e)
+
+  prompt, output_parser = loadFewShot(templateFileName=jobSettings["templateFileName"])
+
+  llm = loadModel(
+      settingsFileName=jobSettings["settingsFileName"],
+      modelPath=jobSettings["modelPath"]
+      )
+
+  for input in jobSettings["inputs"]:
+    for i in range(jobSettings['quantity']):
+        print(f"Generating {i + 1} of {jobSettings['quantity']}")
+        parsed = runPrompt(input=input, prompt=prompt, llm=llm, outputParser=output_parser)
+        if (parsed["prompt"]):
+            if (jobSettings['outputPath']):
+                file1 = open(f"{jobSettings['outputPath']}/{jobSettings['templateFileName'].split('.')[0]}-{input.split()[0]}.txt", "a")
+                file1.write(parsed["prompt"] + "\n")
+                file1.close()
+            else:
+                print(parsed["prompt"])
