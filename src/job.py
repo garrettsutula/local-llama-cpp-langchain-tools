a89322b1bdffd78000a3726e769c45216fd80b85
Garrett Sutula <garrettsutula@gmail.com>
2023-07-08 15:29:55 -0400

    Add multiple settings to job schema

--- src/job.py
+++ src/job.py
@@ -16,21 +16,24 @@ with open(f"./jobs/{args.name}", "r") as stream:
 prompt, output_parser = loadFewShot(templateFileName=jobSettings["templateFileName"])
 
 modelPaths = jobSettings.get("modelPaths") or [jobSettings["modelPath"]]
+settingsFileNames = jobSettings.get("settingsFileNames") or [jobSettings["settingsFileName"]]
 
 for modelPath in modelPaths:
-    llm = loadModel(
-    settingsFileName=jobSettings["settingsFileName"],
-    modelPath=modelPath
-    )
-    for input in jobSettings["inputs"]:
-        print(f"Generating {jobSettings['quantity']}x '{input}'")
-        for i in range(jobSettings['quantity']):
-            print(f"Generating {i + 1} of {jobSettings['quantity']}")
-            parsed = runPrompt(input=input, prompt=prompt, llm=llm, outputParser=output_parser)
-            if (parsed and parsed["prompt"]):
-                if (jobSettings['outputPath']):
-                    file1 = open(f"{jobSettings['outputPath']}/{jobSettings['templateFileName'].split('.')[0]}-{input.split()[0]}.txt", "a")
-                    file1.write(parsed["prompt"] + "\n")
-                    file1.close()
-                else:
-                    print(parsed["prompt"])
+    for settingsFileName in settingsFileNames:
+        print(f"Using settings '{settingsFileName}'")
+        llm = loadModel(
+        settingsFileName=settingsFileName,
+        modelPath=modelPath
+        )
+        for input in jobSettings["inputs"]:
+            print(f"Generating {jobSettings['quantity']}x '{input}'")
+            for i in range(jobSettings['quantity']):
+                print(f"Generating {i + 1} of {jobSettings['quantity']}")
+                parsed = runPrompt(input=input, prompt=prompt, llm=llm, outputParser=output_parser)
+                if (parsed and parsed["prompt"]):
+                    if (jobSettings['outputPath']):
+                        file1 = open(f"{jobSettings['outputPath']}/{jobSettings['templateFileName'].split('.')[0]}-{input.split()[0]}.txt", "a")
+                        file1.write(parsed["prompt"] + "\n")
+                        file1.close()
+                    else:
+                        print(parsed["prompt"])
